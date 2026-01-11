b4c8abc97272883cb073f608c6ce325e48d746a2
jazzalex <alexander.carot@hs-anhalt.de>
2026-01-11 20:37:11 +0100

    recBytes edit

--- ias.cpp
+++ ias.cpp
@@ -35,8 +35,7 @@ static int portAudioCallback( const void *inputBuffer, void *outputBuffer,
 	my->after = (double) ((result.tv_sec*1000) + (((double) result.tv_usec) / 1000));
         my->callbackInterval = my->after - my->before;
   
-        cout << "SAMPLES: " << framesPerBuffer << " EVEN: " << my->even <<  " CALLBACK-INTERVAL: " << my->callbackInterval << endl;
-	//cout << " CALC: " << my->playoutCbck << endl;
+        //cout << "SAMPLES: " << framesPerBuffer << " EVEN: " << my->even <<  " CALLBACK-INTERVAL: " << my->callbackInterval << endl;
     #endif
 
     /// DEFINE INPUT AND OUTPUT BUFFER
